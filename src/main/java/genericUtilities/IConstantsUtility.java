5f570a4d6f23dbb39076261aedce5186ffbf3050
ChaitraQsp <chaitraqspvirtual@gmail.com>
2023-06-14 20:54:44 +0530

    Framework committed

---
+++ src/main/java/genericUtilities/IConstantsUtility.java
@@ -0,0 +1,13 @@
+package genericUtilities;
+
+/**
+ * This interface has all constants
+ * @author Chaitra M
+ *
+ */
+public interface IConstantsUtility {
+	
+	String propertyFilePath = ".\\src\\test\\resources\\CommonData.properties";
+	String excelFilePath = ".\\src\\test\\resources\\TestData.xlsx"; 
+
+}
