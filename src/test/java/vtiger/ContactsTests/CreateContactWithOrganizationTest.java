fbefaf87b33813ec9972cafec95b641fe7232b68
Vivek Nallala <vivekpatel02221@gmail.com>
2025-10-17 14:19:01 +0530

    Comitted for CreateORg

--- src/test/java/vtiger/ContactsTests/CreateContactWithOrganizationTest.java
+++ src/test/java/vtiger/ContactsTests/CreateContactWithOrganizationTest.java
@@ -75,5 +75,7 @@ public class CreateContactWithOrganizationTest extends BaseClass {
     public void demo()
     {
     	System.out.println("This is demo");
+    	System.out.println("Vivek Comitted");
+
     }
 }
