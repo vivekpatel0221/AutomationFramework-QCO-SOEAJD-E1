f9a394ee220c3b39635777cc9fe4ec03ca4a4b0e
Naveen Mogalla <Naveen Mogalla@manukotaabhishek>
2025-10-17 14:22:37 +0530

    commit

--- src/main/java/genericUtilities/RetryAnalyserImplementation.java
+++ src/main/java/genericUtilities/RetryAnalyserImplementation.java
@@ -12,7 +12,7 @@ public class RetryAnalyserImplementation implements IRetryAnalyzer{
 
 	int count = 0;
 	int retryCount = 4; 
-	                                 //fail
+	                                 
 	public boolean retry(ITestResult result) {
 		
 		while(count<retryCount) //0<4/1<4/2<4/3<4/4<4 - no
